f8e0aa3e4d5503978796430975331abb419fcd56
Messias Fernandes <dev.messiasfer@gmail.com>
2025-06-12 20:23:23 -0300

    início do desenvolvimento dos avatares

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Characters from "./components/Characters"
 
 const App = () => {
   return (
---
+++ src/components/Characters.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Characters = () => {
+  return (
+    <div className='relative w-full h-screen overflow-hidden mb-[-10%]'>
+
+      
+
+    </div>
+  )
+}
+
+export default Characters
