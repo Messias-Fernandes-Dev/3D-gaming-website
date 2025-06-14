06bce96826bf736367bcccbf2fe84b6cb8ac39e2
Messias Fernandes <dev.messiasfer@gmail.com>
2025-06-14 20:04:42 -0300

    criação dos status de poder dos avatares, link com objetos

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 const Characters = () => {
+
+  // Track which Avatar is selected
+  const [selectedAvatar, setSelectedAvatar] = useState("VIKI");
+
+  // Simplified Avatar data
+  const Avatar = {
+    VIKI:{
+    name: "VIKI", 
+    power: 75,
+    stable: 95, 
+    penetrate: 30,
+    portable: 80,
+    stars: 3,
+    },
+    EVA: {
+      name: "EVA",
+      power: 90,
+      stable: 0,
+      penetrate: 70,
+      portable: 60,
+      stars: 4,
+    }
+  }
+ 
+  // Get current Avatar data
+  const currentAvatar = Avatar[selectedAvatar];
+
   return (
     <div className="relative w-full h-screen overflow-hidden mb-[10%]">
       {/* Section Title */}
@@ -17,8 +44,9 @@ const Characters = () => {
       <div className="relative z-10 flex md:flex-row flex-col items-center w-full h-full p-4">
         {/* Left side - Avatar info and selection */}
         <div className="w-full md:w-2/4 flex flex-col md:ml-10">
-          {/* Selected Avatar */}
+          {/* Selected Avatar info card*/}
           <div className="bg-gray-900/80 backdrop-blur-sm rounded-lg p-4 mb-4 border border-gray-800 shadow-[0_0_15px_rgba(167,139,250,0.2)]">
+          <h1 className="text-2xl font-semibold mb-2">{currentAvatar.name}</h1>
             {/*  Avatar Statistics */}
             <div className="space-y-3 mb-16">
               {/* Power stats */}
@@ -26,8 +54,12 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Power</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"></div>
+                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{width: `${currentAvatar.power}%`}}
+                  >
+                  </div>
                 </div>
+                <span className="ml-2">{currentAvatar.power}</span>
               </div>
 
               {/* stable stats */}
@@ -35,8 +67,12 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Stable</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"></div>
+                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{width: `${currentAvatar.stable}%`}}
+                  >
+                  </div>
                 </div>
+                <span className="ml-2">{currentAvatar.stable}</span>
               </div>
 
               {/* stable stats */}
@@ -44,8 +80,12 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Penetrate</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"></div>
+                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
+                  style={{width: `${currentAvatar.penetrate}%`}}
+                  >
+                  </div>
                 </div>
+                <span className="ml-2">{currentAvatar.penetrate}</span>
               </div>
 
               {/* stable stats */}
@@ -53,13 +93,37 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Portable</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"></div>
+                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
+                  style={{width: `${currentAvatar.portable}%`}}
+                  >
+                  </div>
                 </div>
+                <span className="ml-2">{currentAvatar.portable}</span>
               </div>
             </div>
 
-            
+            {/* Action Buttons  */}
+            <div className="flex gap-3">
+              <button className="px-4 py-1 bg-violet-900 text-white rounded-md font-semibold hover:opacity-70 transition-all duration-300">
+                Porficient
+              </button>
+
+              <button className="px-4 py-1 bg-violet-900 text-white rounded-md font-semibold hover:opacity-70 transition-all duration-300">
+                Redemption
+              </button>
+            </div>            
           </div>
+
+          {/* Avatar Selection Cards */}
+          <div className="grid grid-cols-2 gap-4">
+            {/* VIKI CARD */}
+            <div className="relative bg-gray-900/70 backdrop-blur-sm rounded-lg p-3 border flex lg:flex-row flex-col justify-between px-12 items-center cursor-pointer transition-all duration-300">
+
+            <div className="text-lg mb-2">Viki</div>
+
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
