f383fc0064d52d80ed7747cc83801dec372c4dac
Messias Fernandes <dev.messiasfer@gmail.com>
2025-06-16 16:06:48 -0300

    finalização dos status e efeitos de escolha dos avatares, usando framer-motion e animate presence

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
-import { Star } from "lucide-react"
+import { Star } from "lucide-react";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Characters = () => {
-
   // Track which Avatar is selected
   const [selectedAvatar, setSelectedAvatar] = useState("VIKI");
 
   // Simplified Avatar data
   const Avatar = {
-    VIKI:{
-    name: "VIKI", 
-    power: 75,
-    stable: 95, 
-    penetrate: 30,
-    portable: 80,
-    stars: 3,
+    VIKI: {
+      name: "VIKI",
+      power: 75,
+      stable: 95,
+      penetrate: 30,
+      portable: 80,
+      stars: 3,
     },
     EVA: {
       name: "EVA",
       power: 90,
-      stable: 0,
+      stable: 80,
       penetrate: 70,
       portable: 60,
       stars: 4,
-    }
-  }
- 
+    },
+  };
+
   // Get current Avatar data
   const currentAvatar = Avatar[selectedAvatar];
 
@@ -47,7 +47,9 @@ const Characters = () => {
         <div className="w-full md:w-2/4 flex flex-col md:ml-10">
           {/* Selected Avatar info card*/}
           <div className="bg-gray-900/80 backdrop-blur-sm rounded-lg p-4 mb-4 border border-gray-800 shadow-[0_0_15px_rgba(167,139,250,0.2)]">
-          <h1 className="text-2xl font-semibold mb-2">{currentAvatar.name}</h1>
+            <h1 className="text-2xl font-semibold mb-2">
+              {currentAvatar.name}
+            </h1>
             {/*  Avatar Statistics */}
             <div className="space-y-3 mb-16">
               {/* Power stats */}
@@ -55,10 +57,10 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Power</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
-                    style={{width: `${currentAvatar.power}%`}}
-                  >
-                  </div>
+                  <div
+                    className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{ width: `${currentAvatar.power}%` }}
+                  ></div>
                 </div>
                 <span className="ml-2">{currentAvatar.power}</span>
               </div>
@@ -68,10 +70,10 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Stable</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
-                    style={{width: `${currentAvatar.stable}%`}}
-                  >
-                  </div>
+                  <div
+                    className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{ width: `${currentAvatar.stable}%` }}
+                  ></div>
                 </div>
                 <span className="ml-2">{currentAvatar.stable}</span>
               </div>
@@ -81,10 +83,10 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Penetrate</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
-                  style={{width: `${currentAvatar.penetrate}%`}}
-                  >
-                  </div>
+                  <div
+                    className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{ width: `${currentAvatar.penetrate}%` }}
+                  ></div>
                 </div>
                 <span className="ml-2">{currentAvatar.penetrate}</span>
               </div>
@@ -94,10 +96,10 @@ const Characters = () => {
                 <span className="w-24 text-gray-400">Portable</span>
 
                 <div className="flex-1 h-4 bg-gray-800 rounded-full overflow-hidden">
-                  <div className="h-full bg-gradient-to-r from-violet-600 to-white"
-                  style={{width: `${currentAvatar.portable}%`}}
-                  >
-                  </div>
+                  <div
+                    className="h-full bg-gradient-to-r from-violet-600 to-white"
+                    style={{ width: `${currentAvatar.portable}%` }}
+                  ></div>
                 </div>
                 <span className="ml-2">{currentAvatar.portable}</span>
               </div>
@@ -112,60 +114,97 @@ const Characters = () => {
               <button className="px-4 py-1 bg-violet-900 text-white rounded-md font-semibold hover:opacity-70 transition-all duration-300">
                 Redemption
               </button>
-            </div>            
+            </div>
           </div>
 
           {/* Avatar Selection Cards */}
           <div className="grid grid-cols-2 gap-4">
             {/* VIKI CARD */}
-            <div className="relative bg-gray-900/70 backdrop-blur-sm rounded-lg p-3 border flex lg:flex-row flex-col justify-between px-12 items-center cursor-pointer transition-all duration-300">
+            <div
+              className="relative bg-gray-900/70 backdrop-blur-sm rounded-lg p-3 border flex lg:flex-row flex-col justify-between px-12 items-center cursor-pointer transition-all duration-300"
+              onClick={() => setSelectedAvatar("VIKI")}
+            >
+              <div className="text-lg mb-2">VIKI</div>
 
-            <div className="text-lg mb-2">Viki</div>
+              {/* Avatar visual placeholder */}
+              <div className="w-20 h-20 bg-gray-800/50 rounded-md flex items-center justify-center mb-2">
+                <img src="public\images\VIKI.png" alt="VIKI-IMG" />
+              </div>
 
-            {/* Avatar visual placeholder */}
-            <div className="w-20 h-20 bg-gray-800/50 rounded-md items-center justify-center mb-2">
-              <img src="public\images\VIKI.png" alt="VIKI-IMG" />
+              {/* Stars rating */}
+              <div className="flex">
+                {[...Array(3)].map((_, i) => (
+                  <Star
+                    key={i}
+                    className="w-4 h-4 fill-violet-400 text-violet-500"
+                  />
+                ))}
+              </div>
+
+              {/* Highlight for selected avatar */}
+              {selectedAvatar === "VIKI" && (
+                <div className="absolute inset-0 border-2 rounded-lg pointer-events-none"></div>
+              )}
             </div>
 
-            {/* Stars rating */}
-            <div className="flex">
-              {[...Array(3)].map((_,i) => (
-                <Star key={i} className="w-4 h-4 fill-violet-400 text-violet-500"/>
-              ))}
-            </div>
+            {/* EVA CARD */}
+            <div
+              className="relative bg-gray-900/70 backdrop-blur-sm rounded-lg p-3 border flex lg:flex-row flex-col justify-between px-12 items-center cursor-pointer transition-all duration-300"
+              onClick={() => setSelectedAvatar("EVA")}
+            >
+              <div className="text-lg mb-2">EVA</div>
 
-            {/* Highlight for selected avatar */}
-            {selectedAvatar === "VIKI" && (
-              <div className="absolute inset-0 border-2 rounded-lg pointer-events-none"></div>
-            )}
+              {/* Avatar visual placeholder */}
+              <div className="w-20 h-20 bg-gray-800/50 rounded-md flex items-center justify-center mb-2">
+                <img src="public\images\EVA.png" alt="EVA-IMG" />
+              </div>
 
-            </div>
+              {/* Stars rating */}
+              <div className="flex">
+                {[...Array(4)].map((_, i) => (
+                  <Star
+                    key={i}
+                    className="w-4 h-4 fill-violet-400 text-violet-500"
+                  />
+                ))}
+              </div>
 
-            {/* VIKI CARD */}
-            <div className="relative bg-gray-900/70 backdrop-blur-sm rounded-lg p-3 border flex lg:flex-row flex-col justify-between px-12 items-center cursor-pointer transition-all duration-300">
-
-            <div className="text-lg mb-2">Viki</div>
-
-            {/* Avatar visual placeholder */}
-            <div className="w-20 h-20 bg-gray-800/50 rounded-md items-center justify-center mb-2">
-              <img src="public\images\VIKI.png" alt="VIKI-IMG" />
-            </div>
-
-            {/* Stars rating */}
-            <div className="flex">
-              {[...Array(3)].map((_,i) => (
-                <Star key={i} className="w-4 h-4 fill-violet-400 text-violet-500"/>
-              ))}
-            </div>
-
-            {/* Highlight for selected avatar */}
-            {selectedAvatar === "VIKI" && (
-              <div className="absolute inset-0 border-2 rounded-lg pointer-events-none"></div>
-            )}
-
+              {/* Highlight for selected avatar */}
+              {selectedAvatar === "EVA" && (
+                <div className="absolute inset-0 border-2 rounded-lg pointer-events-none"></div>
+              )}
             </div>
           </div>
+        </div>
 
+        {/* right side -3d model */}
+        <div className="relative md:w-2/4 w-full md:h-full h-80 flex items-center justify-center overflow-hidden ">
+
+        <AnimatePresence mode="wait">
+          {selectedAvatar === "VIKI" ? (
+            <motion.div
+              key="VIKI"
+              className="absolute inset-0"
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 0.5 }}
+            >
+              <img src="public\images\VIKI.png" alt="" />
+            </motion.div>
+          ) : (
+            <motion.div
+              key="EVA"
+              className="absolute inset-0"
+              initial={{ x: "100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ duration: 0.5 }}
+            >
+              <img src="public\images\EVA.png" alt="" />
+            </motion.div>
+          )}
+        </AnimatePresence>
         </div>
       </div>
     </div>
