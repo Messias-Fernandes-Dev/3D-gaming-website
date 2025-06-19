2dc425f1a7977f3123033c7cc2ea41385e6f6a16
Messias Fernandes <dev.messiasfer@gmail.com>
2025-06-19 19:01:52 -0300

    desenvolvimento do footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import 'boxicons/css/boxicons.min.css';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <img className='h-10' src="public\images\illu-text.png" alt="" />
+
+      <img className='h-16' src="public\images\illu-logo.png" alt="" />
+
+      <div className='flex gap-4 '>
+        <a href="#"></a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
