7ac5bff4c8f9cd63a68f6a5c959ed186ddda398e
Messias Fernandes <dev.messiasfer@gmail.com>
2025-06-21 18:51:03 -0300

    estilização e adição dos icons e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,36 @@
-import 'boxicons/css/boxicons.min.css';
-
+import "boxicons/css/boxicons.min.css";
 
 const Footer = () => {
   return (
-    <footer>
-      <img className='h-10' src="public\images\illu-text.png" alt="" />
+    <footer className="flex items-center justify-between lg:mt-[15%] mt-[25%] py-8 lg-px-32 md:px-16 px-8 border-t-[0.3px] border-[#babaff]">
+      <img className="h-10" src="public\images\illu-text.png" alt="" />
 
-      <img className='h-16' src="public\images\illu-logo.png" alt="" />
+      <img className="h-16" src="public\images\illu-logo.png" alt="" />
 
-      <div className='flex gap-4 '>
-        <a href="#"></a>
+      <div className="flex gap-4 ">
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300 "
+          href="#"
+        >
+          <i class="bx bxl-github"></i>
+        </a>
+
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300 "
+          href="#"
+        >
+          <i class='bx bxs-balloon' ></i>
+        </a>
+
+        <a
+          className="md:text-3xl text-2xl hover:text-violet-600 duration-300 "
+          href="#"
+        >
+         <i class='bx bxl-instagram-alt' ></i>
+        </a>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
